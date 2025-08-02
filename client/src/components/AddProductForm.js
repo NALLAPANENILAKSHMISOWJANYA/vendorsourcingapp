1f3fae9936e8309db31eb34ae5d7e63d4168d786
NALLAPANENI LAKSHMI SOWJANYA <162676564+NALLAPANENILAKSHMISOWJANYA@users.noreply.github.com>
2025-08-02 20:36:32 +0530

    Update AddProductForm.js

--- client/src/components/AddProductForm.js
+++ client/src/components/AddProductForm.js
@@ -15,7 +15,7 @@ function AddProductForm({ onProductAdded }) {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      await axios.post('https://vendorsourcingapp-backend.onrender.com//api/products/add', form, {
+      await axios.post('https://vendorsourcingapp-backend.onrender.com/api/products/add', form, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
